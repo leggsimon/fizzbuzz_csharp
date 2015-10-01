33ff47e79d9519b75300edcffe1d83f978d64b24
Simon Legg <leggsimon@gmail.com>
2015-10-01 14:02:36 +0100

    first test passing

---
+++ fizzbuzz_csharp/FizzBuzz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fizzbuzz_csharp
+{
+	public class FizzBuzz
+	{
+		public List<int> GetFizzBuzz ()
+		{
+			return new List<int> { 1, 2 };
+		}
+	}
+}
+
---
+++ fizzbuzz_csharp/Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace fizzbuzz_csharp
+{
+
+
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestFizzBuzz_ShouldReturnTwoIntegers()
+		{
+			var fizzbuzz = new FizzBuzz ();
+			var expected = new List<int>{1,2};
+		
+			var actual = fizzbuzz.GetFizzBuzz ();
+
+			CollectionAssert.AreEqual (expected, actual);
+		}
+	}
+}
+
